99498b949d74bb0df8779cc5c2864051880aae0f
Michal27DV <michalek.dvork@gmail.com>
2026-06-16 23:02:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    - Firebase API zcela přeskočeno (auth/firestore musí jít vždy live)
    - Verze cache se zvýší při deploy → SW vyhodí starou cache
 */
-const CACHE = 'shift-v5';
+const CACHE = 'shift-v6';
 const PRECACHE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon.svg'];
 
 self.addEventListener('install', (e) => {
